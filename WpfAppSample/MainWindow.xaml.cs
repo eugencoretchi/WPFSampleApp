dfcc3028883cc24e938bfb25b8e491a4ec0368dc
Евгений Корецкий <mcgeenerman@gmail.com>
2023-09-20 16:24:26 +0300

    Learning ControlTemplate
    - added 3 controls as resoruces ControlH, ControlV, ControlG
    - display each of them by click at corresponded buttons:
    - handle button group at OnRBClick main window code

--- WpfAppSample/MainWindow.xaml.cs
+++ WpfAppSample/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows.Documents;
 using System.Windows;
 using System.Collections.Generic;
+using System.Windows.Controls;
 
 namespace WpfAppSample
 {
@@ -13,6 +14,13 @@ namespace WpfAppSample
         {
             InitializeComponent();            
         }
-       
+        private void OnRBClick(object sender, RoutedEventArgs e)
+        {
+            var rb = sender as RadioButton;
+            var tag = rb?.Tag.ToString();
+
+            SelectorControl.Template = this.Resources[tag] as ControlTemplate;
+        }
+
     }
 }
