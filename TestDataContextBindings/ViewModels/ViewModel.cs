af2e01c4dc664e57c96e1d56f2e225e0bd5d816e
Евгений Корецкий <mcgeenerman@gmail.com>
2023-09-22 04:59:27 +0300

    minor cleanup the code

--- TestDataContextBindings/ViewModels/ViewModel.cs
+++ TestDataContextBindings/ViewModels/ViewModel.cs
@@ -15,7 +15,7 @@
         Page3
     }
 
-    internal class MyViewPanel
+    internal struct MyViewPanel
     {
         internal UserControl uc;
         internal ViewModelBase vm;
@@ -23,7 +23,7 @@
 
     internal class ViewModel : ViewModelBase, INotifyPropertyChanged
     {
-        Dictionary<PageNum, MyViewPanel> pages = new Dictionary<PageNum, MyViewPanel>()
+        Dictionary<PageNum, MyViewPanel> pages = new ()
         {
             { PageNum.Page1, new MyViewPanel { uc = new UserControl1(), vm = new ViewModel1() } },
             { PageNum.Page2, new MyViewPanel { uc = new UserControl2(), vm = new ViewModel2() } },
@@ -31,8 +31,6 @@
         };
 
         public UserControl? MainPage { get; set; }
-
-        public string MainPageVisible => MainPage == null ? "Collapsed" : "Visible";
 
         public RelayCommand OnM1Button => new RelayCommand(() => SetPage(PageNum.Page1));
 
@@ -47,12 +45,11 @@
 
         void SetPage(PageNum page)
         {
-            if (pages.TryGetValue(page, out MyViewPanel? panel))
+            if (pages.TryGetValue(page, out MyViewPanel panel))
             {
                 MainPage = panel.uc;
                 MainPage.DataContext = panel.vm;
                 RaisePropertyChanged(nameof(MainPage));
-                RaisePropertyChanged(nameof(MainPageVisible));
             }
         }
     }
